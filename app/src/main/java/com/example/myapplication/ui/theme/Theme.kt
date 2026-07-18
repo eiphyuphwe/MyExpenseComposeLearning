0bb0450f9d4a68b77b16c2fe6becd8ad213f6ebe
eiphyuphwe <eiphyuphwe643@gmail.com>
2026-07-18 14:14:25 +1200

    Delete Unused files

--- app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -3,38 +3,12 @@ package com.example.myapplication.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import com.example.myapplication.ui.taxcalculator.ui.theme.Pink80
-import com.example.myapplication.ui.taxcalculator.ui.theme.Purple80
-import com.example.myapplication.ui.taxcalculator.ui.theme.PurpleGrey80
-
-/*private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)*/
-
-//private val LightColorScheme = lightColorScheme(
-//    primary = Purple40,
-//    secondary = PurpleGrey40,
-//    tertiary = Pink40,
-//
-//    /* Other default colors to override
-//    background = Color(0xFFFFFBFE),
-//    surface = Color(0xFFFFFBFE),
-//    onPrimary = Color.White,
-//    onSecondary = Color.White,
-//    onTertiary = Color.White,
-//    onBackground = Color(0xFF1C1B1F),
-//    onSurface = Color(0xFF1C1B1F),
-//    */
-//)
 private val LightColorScheme = lightColorScheme(
     primary = PurplePrimary,
     onPrimary = TextOnPurple,
